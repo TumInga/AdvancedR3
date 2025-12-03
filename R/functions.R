e480ffb86489366ea1a05fd8e2458423bb57cb9d
Inga Zalia Tummoszeit <inga.tummoszeit@regionh.dk>
2025-12-03 15:54:25 +0100

    glm adding

--- R/functions.R
+++ R/functions.R
@@ -7,11 +7,11 @@
 create_table_descriptive_stats <- function(data) {
   data |>
     dplyr::group_by(metabolite) |>
-    dplyr::summarise(dplyr::across(value, list(mean = mean,median=median, sd = sd, iqr=IQR))) |>
+    dplyr::summarise(dplyr::across(value, list(mean = mean, median = median, sd = sd, iqr = IQR))) |>
     dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), \(x) sprintf("%.1f", round(x, digits = 1)))) |>
     dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
     dplyr::mutate(MeanIQR = glue::glue("{value_median} ({value_iqr})")) |>
-    dplyr::select(Metabolite = metabolite, "Mean SD" = MeanSD,"Mean IQR" = MeanIQR)
+    dplyr::select(Metabolite = metabolite, "Mean SD" = MeanSD, "Mean IQR" = MeanIQR)
 }
 
 #' Plotting the distribution of metabolite
@@ -25,7 +25,7 @@ create_plot_distributions <- function(data) {
     ggplot2::ggplot(ggplot2::aes(x = value)) +
     ggplot2::geom_histogram() +
     ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free") + # it is variable and the scale is free (for each histogram)
-    ggplot2::ggtitle('Histogram of metabolic distribution') +
+    ggplot2::ggtitle("Histogram of metabolic distribution") +
     ggplot2::labs(
       x = "Metabolite Value",
       y = "Count"
@@ -45,4 +45,15 @@ clean <- function(data) {
     dplyr::group_by(dplyr::pick(-value)) |>
     dplyr::summarise(value = mean(value), .groups = "keep") |>
     dplyr::ungroup()
+}
+
+#' Preprocessing: scale the values for each metabolite
+#'
+#' @param data lipidomics and for a specific metabolite
+#'
+#' @returns A data.frame
+
+preprocess <- function(data){
+  data |>
+    dplyr::mutate(class = as.factor(class), value = scale(value))
 }
