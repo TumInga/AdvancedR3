04bac802d235496e6c58be1aa57d78e8b8f43fa9
Inga Zalia Tummoszeit <inga.tummoszeit@regionh.dk>
2025-12-03 11:00:41 +0100

    plotting

--- R/functions.R
+++ R/functions.R
@@ -12,3 +12,11 @@ create_table_descriptive_stats <- function(data) {
     dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
     dplyr::select(Metabolite = metabolite, "Mean SD" = MeanSD)
 }
+
+create_plot_distributions <- function(data) {
+  data |>
+    ggplot2::ggplot(ggplot2::aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free") + # it is variable and the scale is free (for each histogram)
+    ggplot2::theme_minimal()
+}
