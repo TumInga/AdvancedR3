c547566bbace9560e04f535c23976d5a8845aafd
Inga Zalia Tummoszeit <inga.tummoszeit@regionh.dk>
2025-12-03 10:38:09 +0100

    marking table with knitr

--- R/functions.R
+++ R/functions.R
@@ -5,10 +5,10 @@
 #' @returns A data.frame/tibble
 
 create_table_descriptive_stats <- function(data) {
-  data %>%
-    dplyr::group_by(metabolite) %>%
-    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
-    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), \(x) sprintf("%.1f", round(x, digits = 1)))) %>%
-    dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) %>%
+  data |>
+    dplyr::group_by(metabolite) |>
+    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), \(x) sprintf("%.1f", round(x, digits = 1)))) |>
+    dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
     dplyr::select(Metabolite = metabolite, "Mean SD" = MeanSD)
 }
